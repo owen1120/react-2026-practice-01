6e11be2940cd90763ff6ac2d25dca97063708337
Owen He <owen19951120@gmail.com>
2026-01-26 22:21:52 +0800

    Environment ready: Tailwind v4 and Axios installed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-500 underline">
+        Tailwind v4 設定成功！
+      </h1>
+    </div>
+  )
+}
+
+export default App
